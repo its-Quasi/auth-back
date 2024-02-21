febbea2cecd3d92753992dcc14370ee7020f71ec
felipe alferez <alferezfelipe@gmail.com>
2024-02-21 16:21:08 -0500

    add login and create users

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs'
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>
+  ) {
+
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto
+
+      const newUser = new this.userModel({
+        password: bcrypt.hashSync(password, 10),
+        ...userData
+      })
+      await newUser.save()
+      const { password: ignore, ...visibleData } = newUser.toJSON()
+      console.log(visibleData)
+      return visibleData
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists`)
+      }
+      throw new InternalServerErrorException('something went wrong')
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto
+    const user = await this.userModel.findOne({ email })
+    if (!user) {
+      throw new UnauthorizedException('Not Valid Email')
+    }
+    if(!bcrypt.compareSync(password,  user.password)) {
+      throw new UnauthorizedException('Not Valid Password')
+    }
+    const {password:ignore, ...rest} = user.toJSON()
+    
+    return {
+      ...rest,
+      token: 'randomtoken'
+    }
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    findAll() {
+      return `This action returns all auth`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    findOne(id: number) {
+      return `This action returns a #${id} auth`;
+    }
+
+    update(id: number, updateAuthDto: UpdateAuthDto) {
+      return `This action updates a #${id} auth`;
+    }
+
+    remove(id: number) {
+      return `This action removes a #${id} auth`;
+    }
   }
-}
